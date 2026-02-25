f4bb8cd37cb1643fc5b0d3304c97cf0d577e2951
Stefan Guler <stefan.guler@elca.ch>
2026-02-25 10:53:58 +0100

    Initial Quartz site

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "sDev's Docsyard",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
